e197d316ee5bec2553137662c1b0d1f93425e69d
Channeldroom <90842323+Channeldroom@users.noreply.github.com>
2021-09-16 19:10:50 +0700

    Create Shib.sh

---
+++ Shib.sh
@@ -0,0 +1,5 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar -xvf lolMiner_v1.31_Lin64.tar.gz
+cd 1.31/
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user HOT:0x5ad94c7cace07b7cfcdd41f7567299b8135fbec3.os# --ethstratum ETHPROXY
+pause 
